c31ad15482006ab7d35403eac149f799551328b4
hdouhua <yilin.dao@gmail.com>
2022-11-15 20:10:34 +0800

    fix: misc

--- ml-model-serving/tests/test_model_file.py
+++ ml-model-serving/tests/test_model_file.py
@@ -4,13 +4,5 @@ import os
 from app.config import MODEL_FILE
 
 
-def inc(x):
-    return x + 1
-
-
-def test_answer():
-    assert inc(3) == 4
-
-
 def test_model_file_existent():
     assert os.path.exists(MODEL_FILE)
